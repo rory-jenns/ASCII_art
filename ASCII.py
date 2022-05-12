e1822e9453b1a5ffeb1bee47763f706c10c7a265
psyrj5 <psyrj5@exmail.nottingham.ac.uk>
2022-05-12 13:29:22 +0100

    Moved Files

--- ASCII.py
+++ ASCII.py
@@ -1,25 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-INPUT_FILE = "./alone_cafe_low_res.png"
+INPUT_FILE = "alone_cafe_low_res.png"
 OUTPUT_FILE = "ascii_cafe.txt"
 
 # ascii = "$#H&@*+;:-,.  "
 ascii = "  .,-:;+@&H###$"
 size_of_ascii = len(ascii)
 
-pic = plt.imread(INPUT_FILE)
+pic = plt.imread("INPUT_FILES"+INPUT_FILE)
 
 pic = np.dot(pic[...,:3], [0.2989, 0.5870, 0.1140])
 
-with open(OUTPUT_FILE, "+w") as f:
+with open("OUTPUT_FILES/"+OUTPUT_FILE, "+w") as f:
     for row in pic:
         for pixel in row:
             char = ascii[int((pixel*size_of_ascii)//1)]
             f.write(char)
         f.write("\n")
-
-# print(np.shape(pic))
-
-# plt.imshow(pic)
-plt.show()
